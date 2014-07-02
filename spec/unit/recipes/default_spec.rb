f3a45313be88f5c16cd9c4d2e47b1ccb25d38f2b
E Camden Fisher <fish@fishnix.net>
2014-07-01 22:09:15 -0400

    spec update

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,4 +6,13 @@ describe 'memcached::default' do
   it 'installs memcached' do
     expect(chef_run).to install_package('memcached')
   end
+  
+  it 'creates /etc/sysconfig/memcached' do
+    expect(chef_run).to render_file('/etc/sysconfig/memcached').with_content('Sysconfig for memcached')
+  end
+  
+  it 'restarts memcached when /etc/sysconfig/memcached updates' do
+    resource = chef_run.template('/etc/sysconfig/memcached')
+    expect(resource).to notify('service[memcached]').to(:restart).delayed
+  end
 end
